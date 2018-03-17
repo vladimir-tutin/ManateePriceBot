18e7f79ce82277439e644db3f34990af1ef7c7e5
vladimir-tutin <patrick.d.coy@gmail.com>
2018-03-16 20:25:10 -0400

    remove configData.py

--- configData.py
+++
@@ -1,4 +0,0 @@
-credFile = "creds.json"
-sheetName = "Billionaire Flip Club Price Sheet"
-channelID = "420716857637601281"
-discordAuth = "NDE4OTkyNzQyODI0NDExMTQ2.DXpqhQ.usKgr-sKnOBcohTVt3g69SAhIuo"
